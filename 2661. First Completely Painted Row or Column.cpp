5e6bed779734d04977ac979ae6b86177c7ef57df
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-20 18:30:44 +0530

    Update 2661. First Completely Painted Row or Column.cpp

--- 2661. First Completely Painted Row or Column.cpp
+++ 2661. First Completely Painted Row or Column.cpp
@@ -1,4 +1,6 @@
-// BruteForce --GET TLE--
+//Approach - 1 (Brute Force TLE)
+//T.C : O((m*n) * s*(m+n)) , where s = arr.size() which is (m*n)
+//S.C : O(m*n)
 class Solution {
 public:
 
@@ -40,7 +42,9 @@ public:
     }
 };
 
-// Approach--1 Using two array for count row and column
+//Approach - 2 (Better Approach)
+//T.C : O(m*n)
+//S.C : O((m*n) + m + n)
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
@@ -68,5 +72,40 @@ public:
         }
 
         return -1;
+    }
+};
+
+//Approach - 3 (Best Approach)
+//T.C : O(m*n)
+//S.C : O(m*n)
+class Solution {
+public:
+
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        unordered_map<int, int> mp;
+        for(int i=0; i<arr.size(); i++){
+            mp[arr[i]] = i;
+        }
+
+        int minInd = INT_MAX;
+        for(int i=0; i<m; i++){
+            int maxInd = INT_MIN;
+            for(int j=0; j<n; j++){
+                maxInd = max(maxInd, mp[mat[i][j]]);
+            }
+            minInd = min(minInd, maxInd);
+        }
+        for(int i=0; i<n; i++){
+            int maxInd = INT_MIN;
+            for(int j=0; j<m; j++){
+                maxInd = max(maxInd, mp[mat[j][i]]);
+            }
+            minInd = min(minInd, maxInd);
+        }
+
+        return minInd;
     }
 };
