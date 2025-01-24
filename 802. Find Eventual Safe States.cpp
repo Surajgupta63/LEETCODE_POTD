8bdabff3a8b0aaa57581ee30e4d72745e1505aad
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-24 15:49:12 +0530

    Update 802. Find Eventual Safe States.cpp

--- 802. Find Eventual Safe States.cpp
+++ 802. Find Eventual Safe States.cpp
@@ -34,3 +34,51 @@ public:
         return ans;
     }
 };
+
+// Using BFS Kahn's Algorithm (Topological Sort Method)
+// T.C : O(V+E)
+// S.C : O(V) + O(V*E)
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<vector<int>> adj(V);
+        vector<int> indegree(V, 0);
+
+        for(int u=0; u<V; u++){
+            for(int &v : graph[u]){
+                adj[v].push_back(u);
+                indegree[u]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<bool> isSafeNode(V, false);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            isSafeNode[u] = true;
+
+            for(int &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+
+        }
+        vector<int> ans;
+        for(int i=0; i<V; i++){
+            if(isSafeNode[i]){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
