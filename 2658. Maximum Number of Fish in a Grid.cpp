5628e53ae30745902b5c038229bd3bf599fadcaa
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-28 15:42:54 +0530

    Update 2658. Maximum Number of Fish in a Grid.cpp

--- 2658. Maximum Number of Fish in a Grid.cpp
+++ 2658. Maximum Number of Fish in a Grid.cpp
@@ -1,6 +1,6 @@
-// Approach--1 Using BFS
+// Approach-1 Using (BFS)
 // T.C: O(m*n)
-// S.C : O(1)
+// S.C : O(m*n)
 class Solution {
 public:
     typedef pair<int, int> P;
@@ -46,7 +46,7 @@ public:
     }
 };
 
-// Approach--2 Using DFS
+// Approach-2 Using (DFS)
 // T.C: O(m*n)
 // S.C : O(m*n)
 class Solution {
@@ -79,5 +79,68 @@ public:
             }
         }
         return ans;
+    }
+};
+
+// Approach-3 (Using DSU)
+// T.C : O(m*n * alpha(m*n))
+// S.C : O(m*n)
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> fishSize;
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int find(int x){
+        if(x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) return;
+
+        if(fishSize[x_parent] > fishSize[y_parent]){
+            parent[y_parent] = x_parent;
+            fishSize[x_parent] += fishSize[y_parent];
+        }else{
+            parent[x_parent] = y_parent;
+            fishSize[y_parent] += fishSize[x_parent];
+        }
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        parent.resize(m*n);
+        fishSize.resize(m*n, 0);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int idx = i * n + j;
+                parent[idx] = idx;
+                fishSize[idx] = grid[i][j];
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] > 0){
+                    int parent_idx = i * n + j;
+                    for(auto &dir : directions){
+                        int i_ = i + dir[0];
+                        int j_ = j + dir[1];
+                        if(i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && grid[i_][j_] > 0){
+                            int nbr_idx = i_ * n + j_;
+                            Union(parent_idx, nbr_idx);
+                        }
+                    }
+                }
+            }
+        }
+        return *max_element(fishSize.begin(), fishSize.end());
     }
 };
