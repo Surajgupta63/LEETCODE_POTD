934c76a5ea9c50dc45ef22ad25b55f7db1fcc3c4
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-29 13:20:20 +0530

    Update 684. Redundant Connection.cpp

--- 684. Redundant Connection.cpp
+++ 684. Redundant Connection.cpp
@@ -34,3 +34,57 @@ public:
         return {};
     }
 };
+
+//Approach-3 (DSU - Union Find with Path compression)
+//T.C : O(n * alpha(n))
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x){
+        if(x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent) return;
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }else{
+            parent[x_parent] = y_parent;
+            rank[y_parent] += rank[x_parent];
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int V = edges.size();
+        parent.resize(V+1);
+        rank.resize(V+1, 0);
+
+        for(int i=1; i<=V; i++){
+            parent[i] = i;
+        }
+
+        for(auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int u_parent = find(u);
+            int v_parent = find(v);
+
+            if(u_parent == v_parent) return {u, v};
+
+            Union(u, v);
+        }
+        return {};
+    }
+};
