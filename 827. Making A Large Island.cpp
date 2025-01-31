41aa5f5de5b0ce35962f88939b96046b091ce750
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-31 16:54:54 +0530

    Update 827. Making A Large Island.cpp

--- 827. Making A Large Island.cpp
+++ 827. Making A Large Island.cpp
@@ -99,3 +99,67 @@ public:
         return maxArea == 0 ? n*n : maxArea;
     }
 };
+
+//Approach - 3  - Optimal
+//T.C : O(n^2)
+//S.C : O(n^2)
+class Solution {
+public:
+    int n;
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int DFS(vector<vector<int>>& grid, int i, int j, int id){
+        if(i < 0 || i >= n || j < 0 || j >= n || grid[i][j] != 1) return 0;
+
+        grid[i][j] = id;
+        int island = 1;
+
+        for(auto &dir : directions){
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            island += DFS(grid, i_, j_, id);
+        }
+        return island;
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        n = grid.size();
+        unordered_map<int, int> mp;
+        int maxArea = 0;
+        int id = 2;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    int size = DFS(grid, i, j, id);
+                    maxArea = max(maxArea, size);
+                    mp[id] = size;
+                    id++;
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 0){
+                    unordered_set<int> st;
+                    for(auto &dir : directions){
+                        int i_ = i + dir[0];
+                        int j_ = j + dir[1];
+                        if(i_ >= 0 && i_ < n && j_ >= 0 && j_ < n && grid[i_][j_] != 0){
+                            st.insert(grid[i_][j_]);
+                        }
+                    }
+
+                    int size = 1;
+                    for(auto &id : st){
+                        size += mp[id];
+                    }
+                    maxArea = max(maxArea, size);
+                }
+            }
+        }
+        return maxArea;
+    }
+};
