75ed0f58e459fedf21019678cedc0d942e22c901
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-01-31 16:22:11 +0530

    Update 827. Making A Large Island.cpp

--- 827. Making A Large Island.cpp
+++ 827. Making A Large Island.cpp
@@ -47,3 +47,55 @@ public:
         return maxArea == 0 ? n*n : maxArea;
     }
 };
+
+//Approach-2 Better Brute Force
+// T.C : O(n^4) ---> TLE
+// S.C : O(n^2)
+class Solution {
+public:
+    int n;
+    vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int DFS(vector<vector<int>>& grid, int i, int j, vector<vector<bool>> &visited){
+        if(i < 0 || i >= n || j < 0 || j >= n || grid[i][j] == 0 || visited[i][j]) return 0;
+
+        visited[i][j] = true;
+        int island = 1;
+
+        for(auto &dir : directions){
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            island += DFS(grid, i_, j_, visited);
+        }
+        return island;
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        n = grid.size();
+        int maxArea = 0;
+
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    maxArea = max(maxArea, DFS(grid, i, j, visited));
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 0){
+                    grid[i][j] = 1;
+
+                    visited = vector<vector<bool>>(n, vector<bool>(n, false));
+                    maxArea = max(maxArea, DFS(grid, i, j, visited));
+                    
+                    grid[i][j] = 0;
+                }
+            }
+        }
+        return maxArea == 0 ? n*n : maxArea;
+    }
+};
