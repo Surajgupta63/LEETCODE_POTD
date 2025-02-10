85f7be8a9defebb1eeab75e6a83fe6a22780cca6
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-02-10 21:44:41 +0530

    Update 3174. Clear Digits.cpp

--- 3174. Clear Digits.cpp
+++ 3174. Clear Digits.cpp
@@ -25,3 +25,23 @@ public:
         return ans;
     }
 };
+
+// Approach-2 Using String
+// T.C : O(N)
+// S.C : O(1)
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(!ans.empty() && isdigit(s[i])){
+                ans.pop_back();
+            }
+            else ans.push_back(s[i]);
+        }
+
+        if (ans.empty()) return "";
+        return ans;
+    }
+};
