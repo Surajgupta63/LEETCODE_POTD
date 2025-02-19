515bdfb9a5c2d141fbb282bb8b504583fa31aa22
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-02-19 23:16:29 +0530

    Update 1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp

--- 1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
+++ 1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -1,4 +1,4 @@
-//// Approach Backtracking
+//// Approach-1 Backtracking
 // T.C : O(n*2^n)
 // S.C : O(n*2^n)
 class Solution {
@@ -26,5 +26,36 @@ public:
         if(ans.size() < k) return "";
 
         return ans[k-1];
+    }
+};
+
+//// Approach-2 Backtracking
+// T.C : O(n*2^n)
+// S.C : O(n) // Space Optimized
+class Solution {
+public:
+    void solve(string& curr, int& n, string& ans, int& count, int& k){
+        if(curr.size() == n){
+            count++;
+            if(count == k) ans = curr;
+            return;
+        }
+
+        for(char ch ='a'; ch<='c'; ch++){
+            if(!curr.empty() && curr.back() == ch) continue;
+
+            curr.push_back(ch);  // DO
+            solve(curr, n, ans, count, k); // Explore
+            curr.pop_back();     // Undo
+        }
+    }
+    string getHappyString(int n, int k) {
+        string curr = "";
+        string ans = "";
+        int count = 0;
+
+        solve(curr, n, ans, count, k);
+
+        return ans;
     }
 };
