7ad534bef6faf469f0f930dfeb6fd7b6c789bb55
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-02-19 23:48:48 +0530

    Update 1004. Max Consecutive Ones III.cpp

--- 1004. Max Consecutive Ones III.cpp
+++ 1004. Max Consecutive Ones III.cpp
@@ -1,4 +1,4 @@
-//// Approach Sliding Window
+// Approach-1 Sliding Window
 // T.C : O(2n)
 // S.C : O(1)
 class Solution {
@@ -16,6 +16,35 @@ public:
                 if(nums[i] == 0) zeros--;
                 i++;
             }
+            if(zeros <= k){
+                maxSize1 = max(maxSize1, j-i+1);
+            }
+            j++;
+        }
+        return maxSize1;
+    }
+};
+
+//// Approach Sliding Window
+// T.C : O(n) // Optimized Inside while() loop
+// S.C : O(1)
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int i=0, j=0;
+        int zeros = 0;
+        int maxSize1 = 0;
+
+        while(j < n){
+            if(nums[j] == 0) zeros++;
+            
+            if(zeros > k){
+                if(nums[i] == 0) zeros--;
+                i++;
+            }
+
             if(zeros <= k){
                 maxSize1 = max(maxSize1, j-i+1);
             }
