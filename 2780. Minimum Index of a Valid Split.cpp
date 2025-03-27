e8c4815fdc9e29c1318dd8cdc6035e24850f330f
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-03-27 23:26:12 +0530

    Update 2780. Minimum Index of a Valid Split.cpp

--- 2780. Minimum Index of a Valid Split.cpp
+++ 2780. Minimum Index of a Valid Split.cpp
@@ -1,4 +1,4 @@
-// APproach-1
+// Approach-1
 // T.C: O(n)
 // S.C: O(n)
 class Solution {
@@ -24,6 +24,50 @@ public:
             if(leftMap[num]*2 > n1 && rightMap[num]*2 > n2){
                 return i;
             }
+        }
+
+        return -1;
+    }
+};
+
+// Approach-2 (Majority Element -> Boore More Concept)
+// T.C: O(n)
+// S.C: O(1)
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        int maj = -1;
+        int count = 0;
+        for(int &num: nums){
+            if(count == 0){
+                maj = num;
+                count = 1;
+            }else if(num == maj){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        int majCount = 0;
+        for(int &num: nums){
+            if(num == maj){
+                majCount++;
+            }
+        }
+
+        count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == maj){
+                count++;
+            }
+
+            int remMajCount = majCount - count;
+            int n1 = i+1;
+            int n2 = n-i-1;
+
+            if(count*2 > n1 && remMajCount*2 > n2) return i;
         }
 
         return -1;
