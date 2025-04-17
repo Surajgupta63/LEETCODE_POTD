1499d1b8ac24b4f7bf70fe4a067bca813314d9ba
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-04-17 16:34:07 +0530

    Create 2176. Count Equal and Divisible Pairs in an Array.cpp

---
+++ 2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -0,0 +1,18 @@
+// T.C: O(n^2)
+// S.C: O(1)
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] == nums[j] && (i*j) % k == 0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
