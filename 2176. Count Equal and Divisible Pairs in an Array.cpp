4d4dfaf4f1be1beee016693a2130a6bab36374d3
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-04-17 16:44:18 +0530

    Update 2176. Count Equal and Divisible Pairs in an Array.cpp

--- 2176. Count Equal and Divisible Pairs in an Array.cpp
+++ 2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -16,3 +16,42 @@ public:
         return count;
     }
 };
+
+// T.C: O(n * (log(k) + sqrt(k)))
+// S.C: O(n)
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        unordered_map<int, vector<int>> mp;
+        for(int i=0; i<n; i++){
+            mp[nums[i]].push_back(i);
+        }
+
+        unordered_set<int> factorsOfK;
+        for(int f=1; f*f<=k; f++){ // O(sqrt(k))
+            if(k % f == 0){
+                factorsOfK.insert(f);
+                factorsOfK.insert(k/f);
+            }
+        }
+
+        for(auto& [num, indices]: mp){
+            unordered_map<int, int> prevFactor;
+            for(int i: indices){
+                int GCD = gcd(i, k); // O(log(k))
+                int j = k/GCD;
+                count += prevFactor[j];
+
+                for(int f: factorsOfK){ // O(sqrt(k))
+                    if(i % f == 0){
+                        prevFactor[f]++; 
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
