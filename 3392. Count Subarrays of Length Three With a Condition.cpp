5c1760931341de99addbdfb839e6e88f583ca61f
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-04-27 22:48:40 +0530

    Update 3392. Count Subarrays of Length Three With a Condition.cpp

--- 3392. Count Subarrays of Length Three With a Condition.cpp
+++ 3392. Count Subarrays of Length Three With a Condition.cpp
@@ -6,7 +6,7 @@ public:
         int n = nums.size();
         int count = 0;
         for(int i=0; i<n-2; i++){
-            if(nums[i] + nums[i+2] == nums[i+1]/2.0) count++;
+            if(2 * (nums[i] + nums[i+2]) == nums[i+1]) count++;
         }
 
         return count;
