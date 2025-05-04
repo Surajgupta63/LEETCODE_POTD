43660a845656b0bd3fde39411bebe9b63fd8a988
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-04 22:18:59 +0530

    Create 1128. Number of Equivalent Domino Pairs.cpp

---
+++ 1128. Number of Equivalent Domino Pairs.cpp
@@ -0,0 +1,19 @@
+// T.C: O(n)
+// S.C: O(100)
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int n = dominoes.size();
+        vector<int> freq(100, 0);
+        int pairs = 0;
+        
+        for(int i=0; i<n; i++){
+            if(dominoes[i][0] > dominoes[i][1]) swap(dominoes[i][0], dominoes[i][1]);
+            int num = dominoes[i][0] * 10 + dominoes[i][1];
+            pairs += freq[num];
+            freq[num]++;
+        }
+
+        return pairs;
+    }
+};
