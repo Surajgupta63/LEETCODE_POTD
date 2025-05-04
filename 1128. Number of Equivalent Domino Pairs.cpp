873d4ee6075bfff4460bfb0ffcaaeadea78e105b
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-04 22:25:13 +0530

    Update 1128. Number of Equivalent Domino Pairs.cpp

--- 1128. Number of Equivalent Domino Pairs.cpp
+++ 1128. Number of Equivalent Domino Pairs.cpp
@@ -1,4 +1,23 @@
 // T.C: O(n)
+// S.C: O(n)
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int n = dominoes.size();
+        map<pair<int, int>, int> mp;
+        int pairs = 0;
+
+        for(int i=0; i<n; i++){
+            if(dominoes[i][0] > dominoes[i][1]) swap(dominoes[i][0], dominoes[i][1]);
+            pairs += mp[{dominoes[i][0], dominoes[i][1]}];
+            mp[{dominoes[i][0], dominoes[i][1]}]++;
+        }
+
+        return pairs;
+    }
+};
+
+// T.C: O(n)
 // S.C: O(100)
 class Solution {
 public:
