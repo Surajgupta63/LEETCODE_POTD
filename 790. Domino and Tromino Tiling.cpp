897609c680a1fe737c6798c57a5040a533c108fc
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-06 01:00:25 +0530

    Update 790. Domino and Tromino Tiling.cpp

--- 790. Domino and Tromino Tiling.cpp
+++ 790. Domino and Tromino Tiling.cpp
@@ -1,5 +1,5 @@
 // T.C: O(n)
-// S.C: (n)
+// S.C: O(n)
 class Solution {
 public:
     int MOD = 1000000007;
@@ -16,5 +16,25 @@ public:
     int numTilings(int n) {
         memset(dp, -1, sizeof(dp));
         return solve(n);
+    }
+};
+
+// T.C: O(n)
+// S.C: O(n)
+class Solution {
+public:
+    int numTilings(int n) {
+        if(n == 1 || n == 2) return n;
+        vector<int> dp(n+1, 0);
+        int MOD = 1000000007;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+
+        for(int i=4; i<=n; i++){
+            dp[i] = (2 * dp[i-1] % MOD + dp[i-3] % MOD) % MOD;
+        }
+
+        return dp[n];
     }
 };
