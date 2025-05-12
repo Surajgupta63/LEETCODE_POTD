4b2a297374590dc7f966424fa17e217e8184b7bc
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-12 21:44:21 +0530

    Update 2094. Finding 3-Digit Even Numbers.cpp

--- 2094. Finding 3-Digit Even Numbers.cpp
+++ 2094. Finding 3-Digit Even Numbers.cpp
@@ -5,7 +5,6 @@ public:
     vector<int> findEvenNumbers(vector<int>& digits) {
         int n = digits.size(); 
         unordered_set<int> st;
-        vector<string> permu;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 for(int k=0; k<n; k++){
@@ -20,6 +19,40 @@ public:
 
         vector<int> ans(st.begin(), st.end());
         sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+// T.C: O(10*9*8) ~ O(1)
+// S.C: O(10) ~ O(1)
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        int n = digits.size(); 
+        vector<int> ans;
+        vector<int> freq(10, 0);
+        for(int& digit: digits){
+            freq[digit]++;
+        }
+
+        for(int i=1; i<=9; i++){
+            if(freq[i] == 0) continue;
+            freq[i]--;
+            for(int j=0; j<=9; j++){
+                if(freq[j] == 0) continue;
+                freq[j]--;
+                for(int k=0; k<=8; k=k+2){
+                    if(freq[k] == 0) continue;
+                    freq[k]--;
+                    int num = i*100 + j*10 + k;
+                    ans.push_back(num);
+                    freq[k]++;
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
 
         return ans;
     }
