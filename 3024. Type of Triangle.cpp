15b49de6593bdaa5ea36e1c7a5cc7b9414d8ab4f
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-19 15:13:51 +0530

    Create 3024. Type of Triangle.cpp

---
+++ 3024. Type of Triangle.cpp
@@ -0,0 +1,25 @@
+// T.C: O(3)
+// S.C: O(3)
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if((nums[0] + nums[1]) <= nums[2] || (nums[0] + nums[2]) <= nums[1] || (nums[2] + nums[1]) <= nums[0]){
+            return "none";
+        }
+
+        unordered_map<int, int> mp;
+        for(int& num: nums){
+            mp[num]++;
+        }
+
+
+        if(mp.size() == 3){
+            return "scalene";
+        }
+        else if(mp.size() == 2){
+            return "isosceles";
+        }
+
+        return "equilateral";
+    }
+};
