2e84cd2c4328483ce914373aa69383f0566b1fcf
Suraj Gupta <117096149+Surajgupta63@users.noreply.github.com>
2025-05-19 15:22:10 +0530

    Update 3024. Type of Triangle.cpp

--- 3024. Type of Triangle.cpp
+++ 3024. Type of Triangle.cpp
@@ -23,3 +23,23 @@ public:
         return "equilateral";
     }
 };
+
+// T.C: O(1)
+// S.C: O(1)
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if((nums[0] + nums[1]) <= nums[2] || (nums[0] + nums[2]) <= nums[1] || (nums[2] + nums[1]) <= nums[0]){
+            return "none";
+        }
+
+        if(nums[0] == nums[1] && nums[1] == nums[2]){
+            return "equilateral";
+        }
+        else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]){
+            return "scalene";
+        }
+
+        return "isosceles";
+    }
+};
